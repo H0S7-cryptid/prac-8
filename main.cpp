9b5d7eedf8a294167daa819b0ff2dd91fa138a64
host1l3 <timvitkulevskij@gmail.com>
2025-05-25 17:14:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 Catalog InitializeCatalog() {
 	Catalog catalog;
-	Smartphone samePhoneToSearch{ "Apple 12 pro", 74999.99, Apple, White, 6, 3, 128, Apple, iOS, 3, YES };
+	Smartphone samePhoneToSearch{ "Apple 12 pro", 74999.99, Apple, White, 6, 3, 128, Apple, iOS, 3, YES, RUB};
 
-	catalog.addSmartphone("Apple 10 pro", 29999.99, Apple, Black, 4, 3, 64, Apple, iOS, 2, NO);
+	catalog.addSmartphone("Apple 10 pro", 29999.99, Apple, Black, 4, 3, 64, Apple, iOS, 2, NO, RUB);
 	catalog.addSmartphone(samePhoneToSearch);
 
 	return catalog;
@@ -14,7 +14,7 @@ Catalog InitializeCatalog() {
 int main(int argc, char* argv[]) {
 	Catalog catalog = InitializeCatalog();
 
-	Smartphone whatBuyerLikes{ "Apple 12 Pro", 0, Apple, (Color)0, 0, 0, 0, (Manufacturer)0, (Os)0, 0, (Protection)0 };
+	Smartphone whatBuyerLikes{ "Apple 12 Pro", 0, Apple, (Color)0, 0, 0, 0, (Manufacturer)0, (Os)0, 0, (Protection)0, (Mtype)2};
 
 	vector<Smartphone> Selected;
 
